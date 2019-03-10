51374f5b0909dab5833a88b3490365937ec1806a
usama fouad <usamaeltmsah@yahoo.com>
2019-03-10 21:59:04 +0200

    Add files via upload
    Manage all transactions

--- Usecase2block chain/Transaction.py
+++ Usecase2block chain/Transaction.py
@@ -1,1 +1,14 @@
-#transactions
+import json
+from typing import *
+class Transaction:
+
+    def __init__(self,sender1:str,receive1:str,amount1:int):
+        self.sender=sender1
+        self.receiver=receive1
+        self.amount=amount1
+    def toJsonStr(self):
+        return {
+            'sender':self.sender,
+            'receiver':self.receiver,
+            'amount':self.amount
+        }
